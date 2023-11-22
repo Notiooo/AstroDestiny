9e0845dec4edaf465e2bc0c1afe7d3ad40da93d8
Notiooo <dawidgrobert@gmail.com>
2023-11-22 23:35:20 +0100

    Add GameOverScript

---
+++ AstroFixer/Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using TMPro;
+
+public class GameOverScript : MonoBehaviour
+{
+    public static GameOverScript Instance { get; private set; }
+
+    [SerializeField] private TextMeshProUGUI gameOverText;
+    [SerializeField] private Button restartButton;
+    [SerializeField] private Image fadeInBlackImage;
+    [SerializeField] private Image fadeOutBlackImage;
+    [SerializeField] private float fadeDuration = 2f;
+
+    void Awake()
+    {
+        InitializeSingleton();
+    }
+
+    private void InitializeSingleton()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void DisableUIElements()
+    {
+        fadeOutBlackImage.gameObject.SetActive(false);
+        fadeInBlackImage.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        DisableUIElements();
+    }
+
+    public void TriggerGameOver(string message)
+    {
+        gameOverText.text = message;
+        StartCoroutine(FadeToBlack());
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        fadeInBlackImage.gameObject.SetActive(true);
+        float alpha = 0;
+        while (alpha < 1)
+        {
+            alpha += Time.deltaTime / fadeDuration;
+            fadeInBlackImage.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        fadeOutBlackImage.gameObject.SetActive(true);
+        StartCoroutine(FadeoutSecondBlackPanel());
+    }
+
+    IEnumerator FadeoutSecondBlackPanel()
+    {
+        float alpha = 1;
+        while (alpha > 0)
+        {
+            alpha -= Time.deltaTime / fadeDuration;
+            fadeOutBlackImage.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+        fadeOutBlackImage.gameObject.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- AstroFixer/Assets/Scripts/InSpaceVignetteFade.cs
+++ AstroFixer/Assets/Scripts/InSpaceVignetteFade.cs
@@ -7,20 +7,14 @@ using TMPro;
 public class InSpaceVignetteFade : MonoBehaviour
 {
     public PostProcessVolume volume;
-    public Image blackoutPanel;
     private Vignette vignette;
     private Grain grain;
-    public TextMeshProUGUI messageText;
     public float vignetteFadeDuration = 5.0f;
-    public float blackoutFadeDuration = 5.0f;
-    public float textFadeDuration = 2.0f;
 
     private void Start()
     {
         volume.profile.TryGetSettings(out vignette);
         volume.profile.TryGetSettings(out grain);
-        blackoutPanel.color = new Color(0, 0, 0, 0);
-        messageText.color = new Color(messageText.color.r, messageText.color.g, messageText.color.b, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -59,35 +53,7 @@ public class InSpaceVignetteFade : MonoBehaviour
 
         if (targetIntensity == 1f)
         {
-            StartCoroutine(FadeScreenToBlack(blackoutFadeDuration));
+            GameOverScript.Instance.TriggerGameOver("You are lost in a black abyss.\n\n A slow and unpleasant death from lack of oxygen awaits you.");
         }
-    }
-
-    private IEnumerator FadeScreenToBlack(float duration)
-    {
-        float timeElapsed = 0f;
-        while (timeElapsed < duration)
-        {
-            float alpha = Mathf.Lerp(0f, 1f, timeElapsed / duration);
-            blackoutPanel.color = new Color(0, 0, 0, alpha);
-            timeElapsed += Time.deltaTime;
-            yield return null;
-        }
-
-        blackoutPanel.color = new Color(0, 0, 0, 1f);
-        StartCoroutine(FadeTextToFullAlpha(textFadeDuration, messageText));
-    }
-
-    public IEnumerator FadeTextToFullAlpha(float duration, TextMeshProUGUI text)
-    {
-        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
-        float timeElapsed = 0f;
-        while (timeElapsed < duration)
-        {
-            text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Lerp(0f, 1f, timeElapsed / duration));
-            timeElapsed += Time.deltaTime;
-            yield return null;
-        }
-        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
     }
 }
